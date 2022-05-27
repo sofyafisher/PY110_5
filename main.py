e53231fc0828e686f3cb8382944347eebac6e34a
fisch <python0fishers@gmail.com>
2022-05-27 14:15:48 +0300

    добавлено задание6

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tasks.task2 import wave
 from tasks.task3 import wave_inf
 from tasks.task4 import word_value, find_mostvalued
 from tasks.task5 import modify
+from tasks.task6 import roman_to_int
 
 print(conventer([1, 1, 1, 1]))
 print(wave("hello"))
@@ -13,3 +14,5 @@ for i in range(8):
 
 print(find_mostvalued("abc", "mno", "xyz"))
 print(modify("mouse"))
+
+print(roman_to_int("MCMXC"))
